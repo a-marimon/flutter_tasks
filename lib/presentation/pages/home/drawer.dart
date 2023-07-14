18492218fb1aa679b9716edf69f2c5653393ec71
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-07-14 11:01:33 -0400

    drawer de la app

---
+++ lib/presentation/pages/home/drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:my_tasks/data/const.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Drawer(
+      child: SizedBox(
+        height: size.height,
+        child: Column(
+          children: <Widget>[
+            const DrawerHeader(
+              decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/icon.png'))),
+              child: SizedBox(width: double.maxFinite),
+            ),
+            Expanded(
+              child: ListView(
+                  physics: const BouncingScrollPhysics(),
+                  padding: EdgeInsets.zero,
+                  children: nameCounters
+                      .map(
+                        (e) => ListTile(
+                          leading: Icon(
+                            Icons.exposure,
+                            color: e['color'] as Color,
+                          ),
+                          title: Text("Counter ${e['name']}"),
+                          onTap: () {
+                            // Cerrar el drawer y navegar posteriormente hacia la siguiente pagina
+                            // asi evito que al regresar de esta no este abierto aun el drawer
+                            Navigator.pop(context);
+                          },
+                        ),
+                      )
+                      .toList()),
+            ),
+            const Divider(height: 1),
+            ListTile(
+              leading: const Icon(Icons.settings),
+              title: const Text("Settings"),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/presentation/pages/home/home_pages.dart
+++ lib/presentation/pages/home/home_pages.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:my_tasks/data/const.dart';
+import 'package:my_tasks/presentation/pages/home/drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(kAppName),
+      ),
+      drawer: const MyDrawer(),
+      drawerEdgeDragWidth: size.width / 4, //Defino el area donde aceptara el gesto para abrir el drawer
+    );
   }
 }
