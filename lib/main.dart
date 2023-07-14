5c6aa9b37f6db4af5454fc03a25ba6d9d5006731
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-07-14 11:40:06 -0400

    agregando responsibe a la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:my_tasks/device/theme/theme.dart';
 import 'package:my_tasks/presentation/router/go_router.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,6 +19,15 @@ class MyApp extends StatelessWidget {
       darkTheme: darkTheme(context),
       //NAVIGATOR
       routerConfig: GoNavigator.router,
+      builder: (context, child) => ResponsiveBreakpoints.builder(
+        child: child ?? const Placeholder(),
+        breakpoints: [
+          const Breakpoint(start: 0, end: 800, name: MOBILE),
+          const Breakpoint(start: 801, end: 1200, name: TABLET),
+          const Breakpoint(start: 1201, end: 1920, name: DESKTOP),
+          const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+        ],
+      ),
     );
   }
 }
