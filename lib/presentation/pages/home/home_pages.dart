ecddff0c0ae7faa1f1b4c2d300992cc7b03e515e
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-07-14 13:56:58 -0400

    visualizando en el home el widget de grafico de pasel

--- lib/presentation/pages/home/home_pages.dart
+++ lib/presentation/pages/home/home_pages.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:my_tasks/data/const.dart';
+import 'package:my_tasks/presentation/blocs/dash/dash_bloc.dart';
 import 'package:my_tasks/presentation/pages/home/drawer.dart';
+import 'package:my_tasks/presentation/pages/home/widgets/home_widgets.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    //inicio la primera carga para la visualizacion
+    context.read<DashBloc>().add(DashRefreshEvent());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,6 +30,24 @@ class HomePage extends StatelessWidget {
       ),
       drawer: const MyDrawer(),
       drawerEdgeDragWidth: size.width / 4, //Defino el area donde aceptara el gesto para abrir el drawer
+      body: Padding(
+        padding: const EdgeInsets.all(kDefaultPadding / 2),
+        child: StaggeredGrid.count(
+          crossAxisCount: size.width > size.height ? 8 : 4,
+          mainAxisSpacing: 0,
+          crossAxisSpacing: 0,
+          axisDirection: AxisDirection.down,
+          children: [
+            StaggeredGridTile.count(
+              crossAxisCellCount: size.width > size.height ? 4 : 2,
+              mainAxisCellCount: size.width > size.height ? 4 : 2,
+              child: const Card(
+                child: PieChartWidget(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/presentation/pages/home/widgets/pie_chart_widget.dart
+++ lib/presentation/pages/home/widgets/pie_chart_widget.dart
@@ -1,6 +1,7 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_tasks/data/const.dart';
 import 'package:my_tasks/device/utils.dart';
 import 'package:my_tasks/presentation/blocs/dash/dash_bloc.dart';
 import 'package:my_tasks/presentation/widgets/error/error_page.dart';
@@ -41,31 +42,41 @@ class PieChartWidget extends StatelessWidget {
       );
 
   _buildReadyState(DashReadyState state, {required BoxConstraints constraints, required ColorScheme scheme}) {
-    return PieChart(
-      PieChartData(
-        sections: state.list
-            .map(
-              (e) => PieChartSectionData(
-                showTitle: true,
-                titlePositionPercentageOffset: 1.3,
-                badgeWidget: Text(e.current().toString(), style: TextStyle(color: scheme.background)),
-                value: e.current().toDouble(),
-                title: e.name,
-                color: Utils.getCounterColor(e.name),
-                radius: constraints.maxWidth / 4,
-                titleStyle: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
+    return state.list.isEmpty
+        ? const Center(
+            child: Padding(
+              padding: EdgeInsets.all(kDefaultPadding),
+              child: Text(
+                "No register of meters",
+                textAlign: TextAlign.center,
               ),
-            )
-            .toList(),
-        borderData: FlBorderData(show: true),
-        sectionsSpace: 0,
-        centerSpaceRadius: 5,
-        centerSpaceColor: scheme.onBackground,
-      ),
-    );
+            ),
+          )
+        : PieChart(
+            PieChartData(
+              sections: state.list
+                  .map(
+                    (e) => PieChartSectionData(
+                      showTitle: true,
+                      titlePositionPercentageOffset: 1.3,
+                      badgeWidget: Text(e.current().toString(), style: TextStyle(color: scheme.background)),
+                      value: e.current().toDouble(),
+                      title: e.name,
+                      color: Utils.getCounterColor(e.name),
+                      radius: constraints.maxWidth / 4,
+                      titleStyle: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                  .toList(),
+              borderData: FlBorderData(show: true),
+              sectionsSpace: 0,
+              centerSpaceRadius: 5,
+              centerSpaceColor: scheme.onBackground,
+            ),
+          );
   }
 
   _buildErrorState(BuildContext context, DashErrorState state) => MyExceptionErrorWidget(
