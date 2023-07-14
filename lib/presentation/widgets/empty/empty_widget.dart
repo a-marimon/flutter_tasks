089aca25f48b718382a4354fb300a81b3da7ce99
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-07-14 19:22:24 -0400

    Update empty_widget.dart

--- lib/presentation/widgets/empty/empty_widget.dart
+++ lib/presentation/widgets/empty/empty_widget.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 
 class EmptyWidget extends StatelessWidget {
   final BoxConstraints constraints;
+  final Widget? widget;
 
-  const EmptyWidget({Key? key, required this.constraints}) : super(key: key);
+  const EmptyWidget({Key? key, required this.constraints, this.widget}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: constraints.maxWidth,
       height: constraints.maxHeight,
-      child: const Center(
-        child: Text("Empty"),
+      child: Center(
+        child: widget ?? const Text("Empty"),
       ),
     );
   }
