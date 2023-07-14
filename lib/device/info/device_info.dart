55fd82a200b4719428b7c7649c7e55021e38516f
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-07-14 11:38:21 -0400

    Create device_info.dart

---
+++ lib/device/info/device_info.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+
+class DeviceInfo {
+  static DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+
+  static Future<String> get deviceId async {
+    String deviceId = 'Unknown';
+    if (Platform.isAndroid) {
+      final androidInfo = await deviceInfoPlugin.androidInfo;
+      deviceId = '${androidInfo.model}${androidInfo.device}';
+    }
+    if (Platform.isIOS) {
+      final iosInfo = await deviceInfoPlugin.iosInfo;
+      deviceId = iosInfo.identifierForVendor ?? '';
+    }
+    return deviceId;
+  }
+
+  static Future<int> get deviceSDK async {
+    final androidInfo = await deviceInfoPlugin.androidInfo;
+    int deviceSDK = androidInfo.version.sdkInt;
+    return deviceSDK;
+  }
+}
