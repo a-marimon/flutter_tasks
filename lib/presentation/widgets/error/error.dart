5c70c18a0b807d0a32a00173ece86a6ca7b63432
Arian Camejo Carrera <57977658+ariancamejo@users.noreply.github.com>
2023-07-14 19:55:10 -0400

    Update error.dart

--- lib/presentation/widgets/error/error.dart
+++ lib/presentation/widgets/error/error.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_tasks/data/repository/exepctions.dart';
-import 'package:my_tasks/presentation/blocs/dash/dash_bloc.dart';
 import 'package:my_tasks/presentation/widgets/error/error.dart';
 
 export './unknown_error_widget.dart';
 export './error_widget.dart';
 
-buildErrorState(BuildContext context, MyException exception, {bool small = false}) => MyExceptionErrorWidget(
+buildErrorState(BuildContext context, MyException exception, {bool small = false, Function()? callBack, String? detail}) => MyExceptionErrorWidget(
       exception: exception,
-      detail: 'PieChart',
+      detail: detail ?? "No detail",
       small: small,
-      callBack: () {
-        context.read<DashBloc>().add(DashRefreshEvent());
-      },
+      callBack: callBack,
     );
 
-buildUnknownErrorState(BuildContext context, Exception exception, StackTrace stackTrace, {bool small = false}) => UnknownErrorWidget(
+buildUnknownErrorState(BuildContext context, Exception exception, StackTrace stackTrace, {bool small = false, Function()? callBack, String? detail}) => UnknownErrorWidget(
       exception: exception,
       stackTrace: stackTrace,
-      detail: 'PieChart',
+      detail: detail ?? "No detail",
       small: small,
-      callBack: () {
-        context.read<DashBloc>().add(DashRefreshEvent());
-      },
+      callBack: callBack,
     );
